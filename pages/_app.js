fd64031b52f76a44c786318073914fb19f60eb95
David Koleckar <dkoleckar@gmail.com>
2021-11-26 20:01:24 +0100

    Sidebar, init firebase db and auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth, db} from "../firebase-config";
+import Login from "./login";
+import Loading from "../components/Loading";
+import { doc, setDoc, serverTimestamp } from "firebase/firestore"; 
+import {useEffect} from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const [user, loading] = useAuthState(auth);
+
+    useEffect(() => {
+      if (user) {
+        setDoc(doc(db, "users", user.uid), {
+          email: user.email,
+          lastSeen: serverTimestamp(),
+          photoURL: user.photoURL
+        }, {merge: true});
+      }
+    }, [user])
+
+    if(loading) return <Loading />
+
+    if(!user) return <Login />
+
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
